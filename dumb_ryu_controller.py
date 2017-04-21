3b3ff88a27e2e8fa384dc28efcfcb3c0f5a0fd66
Myoung Lim <myoungslim@gmail.com>
2017-04-20 17:34:50 -0700

    try different flow

--- dumb_ryu_controller.py
+++ dumb_ryu_controller.py
@@ -90,6 +90,28 @@ class L2Forwarding(app_manager.RyuApp):
             flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
         datapath.send_msg(mod)
 
+    def send_flow_mod(self, datapath):
+        ofp = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+
+        cookie = cookie_mask = 0
+        table_id = 0
+        idle_timeout = hard_timeout = 0
+        priority = 32768
+        buffer_id = ofp.OFP_NO_BUFFER
+        match = ofp_parser.OFPMatch(in_port=1, eth_dst='ff:ff:ff:ff:ff:ff')
+        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_NORMAL, 0)]
+        inst = [ofp_parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS,
+                                                 actions)]
+        req = ofp_parser.OFPFlowMod(datapath, cookie, cookie_mask,
+                                    table_id, ofp.OFPFC_ADD,
+                                    idle_timeout, hard_timeout,
+                                    priority, buffer_id,
+                                    ofp.OFPP_ANY, ofp.OFPG_ANY,
+                                    ofp.OFPFF_SEND_FLOW_REM,
+                                    match, inst)
+        datapath.send_msg(req)
+
     # This method is called every time an OF_PacketIn message is received by 
     # the switch. Here we must calculate the best action to take and install
     # a new entry on the switch's forwarding table if necessary
@@ -119,7 +141,8 @@ class L2Forwarding(app_manager.RyuApp):
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
             actions = [ofp_parser.OFPActionOutput(out_port)]
-            self.add_flow(datapath, msg.in_port, dst, actions)
+            #self.add_flow(datapath, msg.in_port, dst, actions)
+            self.send_flow_mod(datapath)
             out = ofp_parser.OFPPacketOut(
                 datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
                 actions=actions)

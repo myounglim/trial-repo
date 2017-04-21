fc9d047ec4ff15eafbec9d69e2cf9cd735bf5765
Myoung Lim <myoungslim@gmail.com>
2017-04-20 20:13:38 -0700

    testing sample

--- dumb_ryu_controller.py
+++ dumb_ryu_controller.py
@@ -1,9 +1,11 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER
+# from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.topology.event import EventSwitchEnter, EventSwitchLeave
 from ryu.lib.mac import haddr_to_bin
+from ryu.ofproto import ofproto_v1_3
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
@@ -79,60 +81,148 @@ class L2Forwarding(app_manager.RyuApp):
     def _ev_switch_leave_handler(self, ev):
         print('leave: %s' % ev)
 
-    # from simple_switch.py
-    def add_flow(self, datapath, in_port, dst, actions):
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
         ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
 
-        match = datapath.ofproto_parser.OFPMatch(
-            in_port=in_port, dl_dst=haddr_to_bin(dst))
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
 
-        mod = datapath.ofproto_parser.OFPFlowMod(
-            datapath=datapath, match=match, cookie=0,
-            command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-            priority=ofproto.OFP_DEFAULT_PRIORITY,
-            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
         datapath.send_msg(mod)
 
-    # This method is called every time an OF_PacketIn message is received by 
-    # the switch. Here we must calculate the best action to take and install
-    # a new entry on the switch's forwarding table if necessary
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
         msg = ev.msg
         datapath = msg.datapath
-        ofp = datapath.ofproto
-        ofp_parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocol(ethernet.ethernet)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
 
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
         dst = eth.dst
         src = eth.src
 
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        graph = self.G
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-        # graph.node[dpid]['mactoport'][src] = msg.in_port
-        # print self.get_str_mactoport(graph, dpid)
-        self.mac_to_port[dpid][src] = msg.in_port
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
 
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
-            actions = [ofp_parser.OFPActionOutput(out_port)]
-            self.add_flow(datapath, msg.in_port, dst, actions)
-            out = ofp_parser.OFPPacketOut(
-                datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
-                actions=actions)
-            datapath.send_msg(out)
         else:
-            out_port = ofp.OFPP_FLOOD
-            actions = [ofp_parser.OFPActionOutput(out_port)]
-            out = ofp_parser.OFPPacketOut(
-                datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
-                actions=actions)
-            datapath.send_msg(out)
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
+
+    # from simple_switch.py
+    # def add_flow(self, datapath, in_port, dst, actions):
+    #     ofproto = datapath.ofproto
+    #
+    #     match = datapath.ofproto_parser.OFPMatch(
+    #         in_port=in_port, dl_dst=haddr_to_bin(dst))
+    #
+    #     mod = datapath.ofproto_parser.OFPFlowMod(
+    #         datapath=datapath, match=match, cookie=0,
+    #         command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
+    #         priority=ofproto.OFP_DEFAULT_PRIORITY,
+    #         flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
+    #     datapath.send_msg(mod)
+    #
+    # # This method is called every time an OF_PacketIn message is received by
+    # # the switch. Here we must calculate the best action to take and install
+    # # a new entry on the switch's forwarding table if necessary
+    # @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    # def packet_in_handler(self, ev):
+    #     msg = ev.msg
+    #     datapath = msg.datapath
+    #     ofp = datapath.ofproto
+    #     ofp_parser = datapath.ofproto_parser
+    #
+    #     pkt = packet.Packet(msg.data)
+    #     eth = pkt.get_protocol(ethernet.ethernet)
+    #
+    #     dst = eth.dst
+    #     src = eth.src
+    #
+    #     dpid = datapath.id
+    #     self.mac_to_port.setdefault(dpid, {})
+    #
+    #     graph = self.G
+    #
+    #     # graph.node[dpid]['mactoport'][src] = msg.in_port
+    #     # print self.get_str_mactoport(graph, dpid)
+    #     self.mac_to_port[dpid][src] = msg.in_port
+    #
+    #     if dst in self.mac_to_port[dpid]:
+    #         out_port = self.mac_to_port[dpid][dst]
+    #         actions = [ofp_parser.OFPActionOutput(out_port)]
+    #         self.add_flow(datapath, msg.in_port, dst, actions)
+    #         out = ofp_parser.OFPPacketOut(
+    #             datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
+    #             actions=actions)
+    #         datapath.send_msg(out)
+    #     else:
+    #         out_port = ofp.OFPP_FLOOD
+    #         actions = [ofp_parser.OFPActionOutput(out_port)]
+    #         out = ofp_parser.OFPPacketOut(
+    #             datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
+    #             actions=actions)
+    #         datapath.send_msg(out)
 
             # att = nx.get_node_attributes(self.ST, 'ports')
             # for neighbor, port in att[dpid].iteritems():

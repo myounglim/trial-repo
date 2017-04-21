3858366cf5c2ea785d626ba9ab687c6dc1c33ab0
Myoung Lim <myoungslim@gmail.com>
2017-04-20 17:05:45 -0700

    try again without flooding

--- dumb_ryu_controller.py
+++ dumb_ryu_controller.py
@@ -127,14 +127,13 @@ class L2Forwarding(app_manager.RyuApp):
         else:
             out_port = ofp.OFPP_FLOOD
             att = nx.get_node_attributes(self.ST, 'ports')
-            #print att[dpid]
             for neighbor, port in att[dpid].iteritems():
-                print neighbor, port
-            #     actions = [ofp_parser.OFPActionOutput(port)]
-            #     out = ofp_parser.OFPPacketOut(
-            #         datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
-            #         actions=actions)
-            #     datapath.send_msg(out)
+                #print neighbor, port
+                actions = [ofp_parser.OFPActionOutput(port)]
+                out = ofp_parser.OFPPacketOut(
+                    datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
+                    actions=actions)
+                datapath.send_msg(out)
 
         # actions = [ofp_parser.OFPActionOutput(out_port)]
 
